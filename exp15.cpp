14fdedc5974111e40608addb8024d8b50aeac95d
Swayam Patel <123808643+Swayam-patel19@users.noreply.github.com>
2025-04-24 23:13:02 +0530

    Add files via upload

---
+++ exp15.cpp
@@ -0,0 +1,92 @@
+
+#include<iostream> 
+using namespace std;
+
+// Creating a function. 
+
+int fact(int n){
+    if(n<=1){
+        return 1;
+    }
+    else{
+        return(n*fact(n-1));  // Recursion 
+    }
+}
+
+int main(){
+    int X,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    X=fact(n);
+    cout<<X;
+    return 0;
+}
+
+// EXPERIMENT - 15(B)
+#include<iostream> 
+using namespace std;
+
+int add(int n){
+    if(n<=1){
+        return 1;
+    }
+    else{
+        return(n+add(n-1));
+    }
+}
+
+int main(){
+    int X,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    X=add(n);
+    cout<<X;
+    return 0;
+}
+
+// EXPERIMENT - 15(C)
+#include<iostream>
+using namespace std;      
+
+// Creating function. 
+void reverse(char *str)
+{
+    if(*str)  // Base Condition 
+    {
+        reverse(str+1);  // Recursion 
+        cout<<("%c",*str);
+    }
+}
+
+int main() 
+{
+    char a[50];
+    cout<<"Enter a string: ";
+    cin>>a;
+    reverse(a);  // Function calling 
+    return 0; 
+}      
+
+// EXPERIMENT - 14(D)
+
+#include<iostream> 
+using namespace std;
+
+// Creating a function 
+
+void print_rev(int i){
+    if(i>0)          // Base Condition  
+    {
+        cout<<(i%10);
+        print_rev(i/10);   // Recursion 
+    }
+}
+
+int main()
+{
+    int i;
+    cout<<"Enter the number: ";
+    cin>>i;
+    print_rev(i);  // Function Calling   
+    return 0;
+}           
